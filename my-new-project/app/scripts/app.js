1f36ff52bbc3dc85c7539509b1d6605e6240d48f
isurridge <isurridgePlex@gmail.com>
2016-01-29 14:27:44 -0800

    first push

---
+++ my-new-project/app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name meetingsBaselineApp
+ * @description
+ * # meetingsBaselineApp
+ *
+ * Main module of the application.
+ */
+angular.module('meetingsBaselineApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
---
+++ my-new-project/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meetingsBaselineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the meetingsBaselineApp
+ */
+angular.module('meetingsBaselineApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
